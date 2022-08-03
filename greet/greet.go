2a956e34cef3695d8ada207b7d25de043401f06e
diovanavalim <diovana.valim@mercadolivre.com>
2022-08-03 14:12:33 -0300

    initial commit

---
+++ greet/greet.go
@@ -0,0 +1,8 @@
+package greet
+
+import "fmt"
+
+// HiThere function says hi using person's name
+func HiThere(name string) {
+	fmt.Printf("Hi there, %s!\n", name)
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "modulo/greet"
+
+func main() {
+	greet.HiThere("Diovana")
+}
